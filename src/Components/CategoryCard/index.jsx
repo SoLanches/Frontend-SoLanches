8f6533818ac0c168f6ae61819a113919cafa4c1b
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2021-08-02 16:59:17 -0300

    adiciona refatoraçao de categoryCard

--- src/Components/CategoryCard/index.jsx
+++ src/Components/CategoryCard/index.jsx
@@ -1,35 +1,29 @@
 import React, { useState} from 'react';
-import './style.css'
+import style from './style.css'
 import Dropdown from './Dropdown'
 
 
 function CategoryCard({title, imageUrl}) {
-    const [setActive, setActiveState] = useState("");
-    const [setRotate, setRotateState] = useState("dropdown__icon")
-    const [setColorCard, setColorCardState] = useState("category-card")
-    const [setColorFont, setColorFontState] = useState("#title")
-    const [setColorIcon, setColorIconState] = useState("#title")
+    const [active, setActive] = useState(false);
+
 
     function toggle(){
-        setActiveState(setActive === "" ? "active" : "")
-        setRotateState(setActive === "active" ? "dropdown__icon" : "dropdown__icon rotate" )
-        setColorCardState(setActive === "active" ? "category-card" : "category-card-white" )
-        setColorFontState(setActive === "active" ? "title" : "title-blue" )
-        setColorIconState(setActive === "active" ? "#fff" : "#FF983B" )
+        setActive(!active)
 
     }
 
     return (
-        <div className={`${setColorCard}`}>
-            <button className={`button-container ${setActive}`} onClick={toggle}>
+        <div className={`${active ? "category-card" : "category-card-white"}`}>
+            <button className={`button-container ${active}`} onClick={toggle}>
             <div className="image-container">
                 <div className="circle">
                     <img src={imageUrl} alt=""/>
                 </div>
             </div>
             </button>
-            <p className={`${setColorFont}`}>{title}</p>
-                <Dropdown className={`${setRotate}`} color={`${setColorIcon}`}/>
+            <p className={`${active ? "title" : "title-blue"}`}>{title}</p>
+                <Dropdown className={`${active ? "dropdown__icon" : "dropdown__icon rotate"}`} 
+                color={`${active ? "#FFF" : "#FF983B" }`}/>
             </div>
     )
 }
