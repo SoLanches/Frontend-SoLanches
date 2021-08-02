da72bf356b3efd18e7f9a170176fe430def41a19
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2021-08-02 17:08:36 -0300

    adiciona rename .css

--- src/Components/CategoryCard/dropdown.jsx
+++ src/Components/CategoryCard/dropdown.jsx
@@ -5,7 +5,6 @@ function Dropdown(props) {
         <svg className= {props.className} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M6 9L12 15L18 9" stroke={props.color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
-
     )
 }
 
--- src/Components/CategoryCard/index.jsx
+++ src/Components/CategoryCard/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState} from 'react';
-import style from './style.css'
+import './style.module.css'
 import Dropdown from './Dropdown'
 
 
