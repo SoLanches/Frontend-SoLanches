0e5154e337de86160dc0d702e17d3e2c5c07a526
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2021-08-04 20:38:48 -0300

    adiciona useState ao componente

--- src/Components/CategoryTags/index.jsx
+++ src/Components/CategoryTags/index.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import style from './style.module.css'
 
 export const CategoryTags= ({title}) => {
 
+    const[active, setActive] = useState(false)
+
+    const handleClick = useCallback(
+        () =>{
+            setActive(!active)
+        }
+    )
+
     return (
-        <div className={style.categoryTag}>
+        <div className={`${style.categoryTag} ${active ? ` ${style.active}`: ""}`} onClick={handleClick}>
             <p className={style.title}>{title}</p>
         </div>
     )
 }
+
