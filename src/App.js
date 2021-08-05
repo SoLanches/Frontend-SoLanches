936de7476c8f5e9826f855565ede0b5b7dd98dfe
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2021-08-04 21:18:52 -0300

    adiciona implementaçao categoryTags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { CategoryTags } from './Components/CategoryTags'
 import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <CategoryTags title="Pizza"/>
+
     </div>
   );
 }
