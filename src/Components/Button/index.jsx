81ab394614c3ef344f24ca40b6a49ba8c1b75057
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2021-08-05 20:07:52 -0300

    adiciona props handleClick ao componente

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from './style.module.css'
 
-export const Button= ({title, handleClick}) => {
+export const Button = ({title, handleClick}) => {
     return (
         <button className={style.container} onClick={handleClick}>
             <p className={style.title}>{title}</p>
