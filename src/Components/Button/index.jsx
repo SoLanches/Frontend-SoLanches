3805a2c0b301e4dc0a65b98ae6ef7227b69750ca
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2021-08-07 20:16:45 -0300

    adiciona responsividade ao componente

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import style from './style.module.css'
 
-export const Button = ({title, handleClick}) => {
+export const Button = ({title, handleClick, icon}) => {
     return (
         <button className={style.container} onClick={handleClick}>
             <p className={style.title}>{title}</p>
+            <img className={style.icon} src={icon}/>
         </button>
     )
 }
