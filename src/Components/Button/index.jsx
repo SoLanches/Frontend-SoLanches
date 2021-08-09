83e443679eae3015a20dcb46f7f40d02ebb98a03
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2021-08-09 02:22:42 -0300

    adiciona opçao de uso de icone juntamente com texto

--- src/Components/Button/index.jsx
+++ src/Components/Button/index.jsx
@@ -4,8 +4,10 @@ import style from './style.module.css'
 export const Button = (props) => {
     return (
         <button className={style.container} onClick={props.handleClick}>
-            <p className={style.title}>{props.title}</p>
             <img className={style.icon} src={props.icon}/>
+            <p className={style.title}>{props.title}</p>
+
         </button>
+
     )
 }
