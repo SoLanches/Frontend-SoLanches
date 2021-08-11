7a0cc633aa0a4e79c116a7809d43d658040fad82
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2021-08-11 00:48:17 -0300

    adiciona indentaçao correta

--- src/Components/CardCommerce/index.jsx
+++ src/Components/CardCommerce/index.jsx
@@ -8,28 +8,27 @@ export const CardCommerce = (props) => {
     return (
         <div className={style.container}>
             <Link to={props.router}>
-            <img className={style.icon} src={props.icon} alt="Logo do comercio" />
-            <p className={style.title}>{props.title}</p>
-            <hr className={style.line}></hr>
-            <div className={style.containerInfos}>
-                <span className={style.location}><Location size="1.2rem" color="#FF983B" /></span>
-                <p className={style.adress}>{props.adress}</p>
-            </div>
-            <div className={style.containerInfos}>
-                <span className={style.infos}><Time  size="1.15rem" color="#FF983B" /></span>
-                <div>
-                    <p className={style.times}>{props.time}</p>
-                    <p className={style.times}>{props.timeTwo}</p>
-                    <p className={style.times}>{props.timeThree}</p>
-                    <p className={style.times}>{props.timeFour}</p>
-                    <p className={style.times}>{props.timeFive}</p>
-                    <p className={style.times}>{props.timeSix}</p>
-                    <p className={style.times}>{props.timeSeven}</p>
+                <img className={style.icon} src={props.icon} alt="Logo do comercio" />
+                <p className={style.title}>{props.title}</p>
+                <hr className={style.line}></hr>
+                <div className={style.containerInfos}>
+                    <span className={style.location}><Location size="1.2rem" color="#FF983B" /></span>
+                    <p className={style.adress}>{props.adress}</p>
                 </div>
-            </div>
+                <div className={style.containerInfos}>
+                    <span className={style.infos}><Time size="1.15rem" color="#FF983B" /></span>
+                    <div>
+                        <p className={style.times}>{props.time}</p>
+                        <p className={style.times}>{props.timeTwo}</p>
+                        <p className={style.times}>{props.timeThree}</p>
+                        <p className={style.times}>{props.timeFour}</p>
+                        <p className={style.times}>{props.timeFive}</p>
+                        <p className={style.times}>{props.timeSix}</p>
+                        <p className={style.times}>{props.timeSeven}</p>
+                    </div>
+                </div>
             </Link>
         </div>
-        
     )
 }
 
