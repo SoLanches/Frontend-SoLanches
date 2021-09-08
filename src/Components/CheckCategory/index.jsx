d85d75535cb36cd3e62c885d1aaa55e52df0f549
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2021-09-08 12:18:59 -0300

    Refatorando componente

--- src/Components/CheckCategory/index.jsx
+++ src/Components/CheckCategory/index.jsx
@@ -1,11 +1,18 @@
 import styles from "./styles.module.css";
+import { useState } from 'react';
 
 export function CheckCategory(props) {
+    const [isSelected, setIsSelected] = useState(false);
+    const handleSelect = (event) => {
+        event.preventDefault();
+        setIsSelected(!isSelected);
+        console.log(isSelected);
+    }
     return (
         <div className={styles.container}>
-            <label className={styles.categoryCheck}>
-                <input type="checkbox"/>
-                <span class={styles.check}></span>
+            <label className={styles.categoryCheck} onClick={handleSelect}>
+                <input type="checkbox" className={isSelected ? '' : styles.isChecked} />
+                <span className={styles.check}></span>
                 <span className={styles.categoryName}>{props.categoryName}</span>
             </label>
         </div>
