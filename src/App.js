1d23efc7e36335c9dc02f66a4cc06e6681ee8e2a
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2021-09-10 00:45:55 -0300

    adiciona primeira versao de teste

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import { Header } from './Components/Header';
+import { CategoryCard } from './Components/CategoryCard';
+import  icon from './assets/images/lunch.svg' ;
 import usePagesContext from './contexts/pages.context';
 import './styles/App.css';
 
@@ -11,6 +13,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
+        <CategoryCard title="Todas" imageUrl={icon} />
         <div className='body'>
           <Switch>
             {pages.map((page, index) => <Route key={index} path={page.path} component={page.component} />)}
