6179dee5858abc1b9582e817729a427d8555ae6a
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2021-09-15 19:23:01 -0300

    adiciona teste do componente check category

--- cypress/integration/components/check-category.spec.js
+++ cypress/integration/components/check-category.spec.js
@@ -1,9 +1,18 @@
 describe("Testa comportamento do componente Check Category", () => { 
-    it("Retorna true se o componente está visível", () => {
-        
+    it("Retorna se o componente está visível", () => {
         cy.visit('/')
         cy.get('.styles_container__MnfE_').should("be.visible")
         cy.get('.styles_container__MnfE_ label').should("be.visible")
+        cy.get('.styles_categoryName__14xRg').should('be.visible')
+
+    })
+
+    it("Retorna se o componente tem uma classe chamada isChecked", () => {
         cy.get('.styles_container__MnfE_ label input').should('have.class', 'styles_isChecked__m-a4l')
+    })
+
+    it("Retorna se, após clicar no botão, a classe isChecked some", () => {
+        cy.get('.styles_container__MnfE_ label').click()
+        cy.get('.styles_isChecked__m-a4l').should('not.exist')
     })
 })
