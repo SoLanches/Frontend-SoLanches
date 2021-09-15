e6c33b3821604fffb1f747661785cc8d8f182f4c
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2021-09-15 19:00:47 -0300

    inserçao do html sem estilos

---
+++ src/pages/PhotoRegister/index.jsx
@@ -0,0 +1,23 @@
+import { ProfileImage } from '../../Components/ProfileImage';
+import { Button } from '../../Components/Button';
+
+export function PhotoRegister() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h1>Construa seu perfil no SoLanches</h1>
+                <span>Adicione informações do seu estabelecimento</span>
+            </div>
+            <div className={styles.inputField}>
+                <ProfileImage />
+                <span>Clique no ícone acima para fazer o envio de uma foto de sua logo</span>
+            </div>
+            <div className={styles.buttons}>
+                <a href="/">
+                    Voltar
+                </a>
+                <Button title="Avançar" />
+            </div>
+        </div>
+    )
+}
