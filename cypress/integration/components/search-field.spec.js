bba19f4469539b6f76c132944f42383ad5345bf7
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2021-09-16 22:46:45 -0300

    teste de componente search field

---
+++ cypress/integration/components/search-field.spec.js
@@ -0,0 +1,19 @@
+describe("Testa comportamento do componente SearchInput", () => { 
+    it("Retorna se o componente está visível", () => {
+        cy.visit('/')
+        cy.get('.styles_container__IveJR').should("be.visible")
+        cy.get('.styles_container__IveJR button').should("be.visible")
+        cy.get('.styles_container__IveJR input').should("be.visible")
+
+    })
+
+    it("Testa o funcionamento entrada", () => {
+        cy.get('.styles_container__IveJR input').type('alá rodrigo do lindo olhar').wait(750)
+        cy.get('.styles_container__IveJR input').should('have.value', 'alá rodrigo do lindo olhar')
+    })
+    
+    it("Testa apagando a entrada se realmente fica vazia", () => {
+        cy.get('.styles_container__IveJR input').clear()
+        cy.get('.styles_container__IveJR input').should('not.have.value', 'alá rodrigo do lindo olhar')
+    })
+})
