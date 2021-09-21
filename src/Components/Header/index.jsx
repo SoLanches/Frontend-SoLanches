decfc6f7902bff17d12acd526b725e3cb1947c12
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-09-21 20:46:57 -0300

    refatora componentes de header e loginCard

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -2,19 +2,23 @@ import React, { useState, useEffect, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router-dom'
 
+import { Button } from '../Button'
 import usePagesContext from '../../contexts/pages.context'
 
 import { ReactComponent as Logo } from '../../assets/icons/logo.svg'
 
 import style from './style.module.css'
+import { LoginCard } from '../LoginCard'
 
 const Header = () => {
-  const { pathname, handlePathname, pages } = usePagesContext()
+  const { pathname, handlePathname } = usePagesContext()
   const [lastScrollTop, setScrollTop] = useState(window.pageYOffset)
   const [retracted, setRetract] = useState(false)
   // TODO: Mudar para quando tiver login adaptando código para o componente saber se o usuario está logado.
   // eslint-disable-next-line no-unused-vars
   const [logged, setLogged] = useState(false)
+
+  const [activeLoginModal, setActiveLoginModal] = useState(false)
 
   const handleScroll = useCallback(() => {
     const currentScrolTop = window.pageYOffset
@@ -36,27 +40,40 @@ const Header = () => {
             <Logo onClick={handlePathname} className={style.logo} />
           </Link>
         </div>
-        <ul className={style.paginas}>
-          {pages.map((page, index) => {
-            if (
-              (page.logged === undefined || logged === page.logged) &&
-              page.header
-            ) {
-              return (
-                <li key={index} onClick={handlePathname}>
-                  <Link
-                    to={page.path}
-                    className={pathname === page.path ? style.active : ''}
-                  >
-                    {page.text}
-                  </Link>
-                </li>
-              )
-            }
-            return null
-          })}
-        </ul>
+
+        <div className={style.paginas}>
+          <Link
+            to={'/inicio'}
+            className={pathname === '/inicio' ? style.active : ''}
+          >
+            {'Página inicial'}
+          </Link>
+
+          <Link
+            to={'/categorias'}
+            className={pathname === '/categorias' ? style.active : ''}
+          >
+            {'Categorias'}
+          </Link>
+
+          {logged ? (
+            <Link
+              to={'/categorias'}
+              className={pathname === '/perfil' ? style.active : ''}
+            >
+              {'Meu perfil'}
+            </Link>
+          ) : (
+            <Button
+              title='Login'
+              handleClick={() => setActiveLoginModal(true)}
+            />
+          )}
+        </div>
       </div>
+      {activeLoginModal && (
+        <LoginCard handleClose={() => setActiveLoginModal(false)} />
+      )}
     </div>
   )
 }
