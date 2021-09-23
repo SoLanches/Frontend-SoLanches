7d98f1af8daa22782134f1012ffd7cb010db0edc
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2021-09-23 18:06:15 -0300

    Retirando console log e detalhes de estilização

--- src/pages/Register/RegisterCategory/index.jsx
+++ src/pages/Register/RegisterCategory/index.jsx
@@ -108,7 +108,6 @@ export function RegisterCategory() {
         </div>
         <div className={styles.categoryList}>
           {categories.map((category, index) => {
-            console.log("Oi")
             return (
               <div key={category} onClick={() => toggleSelect(category)}>
                 <CheckCategory key={category} categoryName={category} />
