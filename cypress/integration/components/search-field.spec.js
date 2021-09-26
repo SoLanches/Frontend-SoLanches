791a8d073891dc08f58cb423e1334b308e80111c
Luciano Erick Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2021-09-25 21:16:48 -0300

    Update cypress/integration/components/search-field.spec.js
    Co-authored-by: Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>

--- cypress/integration/components/search-field.spec.js
+++ cypress/integration/components/search-field.spec.js
@@ -8,7 +8,8 @@ describe("Testa comportamento do componente SearchInput", () => {
     })
 
     it("Testa o funcionamento entrada", () => {
-        cy.get('.styles_container__IveJR input').type('alá rodrigo do lindo olhar').wait(750)
+cy.get('.styles_container__IveJR input').type('alá rodrigo do lindo olhar', {delay: 750});
+
         cy.get('.styles_container__IveJR input').should('have.value', 'alá rodrigo do lindo olhar')
     })
     
